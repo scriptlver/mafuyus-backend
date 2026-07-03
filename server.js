8f0867d23deee73773d8c4f75d0bd7f1594f1196
lavi <165945697+scriptlver@users.noreply.github.com>
2026-07-02 22:23:43 -0300

    a

--- server.js
+++ server.js
@@ -16,6 +16,8 @@ connectDB();
 
 app.use("/cards", cardRoutes);
 
-app.listen(3000, () => {
-  console.log("Servidor rodando");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
